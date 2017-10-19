9755b871419cde824b84ba766f9c7a76c4d29f3f
Leonid Verhovskij <info@leonv.de>
2017-10-19 17:18:40 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,6 @@ nohup sh /usr/sbin/bluespice-up-down-jobs.sh > /dev/null 2>&1 &
 
 service cron start
 service tomcat8 start
-#sleep 5 #wait for tomcat service - solr
 /usr/sbin/bluespice-install.sh
 service memcached start
 /usr/sbin/apache2ctl -D FOREGROUND
